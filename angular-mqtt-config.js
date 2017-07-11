1e3837a4bcae05f36c22035cd6104a17cfabccd7
anhtd <anhtd@softdreams.vn>
2017-07-11 14:57:59 +0700

    g

--- angular-mqtt-config.js
+++ angular-mqtt-config.js
@@ -1,10 +1,55 @@
 (function () {
   'use strict';
+
   angular
     .module('webClient')
-    .config(['MQTTProvider',function(MQTTProvider){
-      MQTTProvider.setHref('ws://test.mosquitto.org:8080/mqtt');
-      console.log("vao cmnr");
+    .config(['$provide', function($provide){
+      $provide.provider('MQTT', function(){
+
+        var settings = {
+          href: ""
+        };
+
+        this.setHref = function(href){
+          settings.href = href;
+        };
+        this.$get = function() {
+          return settings;
+        };
+      });
     }])
-  ;
+
+    .service('MQTTService',
+      ['$q', 'MQTT', function($q, MQTT) {
+        var Service = {};
+        var callbacks = {};
+
+        var client = mqtt.connect(MQTT.href); // you add a ws:// url here
+
+        client.on("message", function(topic, payload) {
+          try {
+            var data = JSON.parse(payload.toString());
+          }catch (e){
+            throw new Error("received data can not parse for JSON !");
+          }
+          angular.forEach(callbacks,function(callback, name){
+            if(name === topic){
+              callback(data);
+            }
+          })
+        });
+
+        client.publish("time", (new Date()).getDate());
+
+        Service.on = function(name, callback){
+          callbacks[name] = callback;
+          client.subscribe(name);
+        };
+        Service.send = function(name, data){
+          client.publish(name, JSON.stringify(data));
+        };
+        return Service;
+      }]);
+
 })();
+
